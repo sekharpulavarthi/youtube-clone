5da0093ad09167f9f32415002a1b15feb139b5e2
sekharpulavarthi <sekharpulavarthi@gmail.com>
2023-11-30 14:25:52 +0530

    implemented youtube

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import "./App.css";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import store from "./utils/stores/store";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import MainContainer from "./components/MainContainer";
-import { Outlet } from "react-router-dom";
 import SearchResults from "./components/SearchResults";
 
 const App = () => {
@@ -40,7 +39,7 @@ export const appRouter = createBrowserRouter([
           {
             path: "/watch",
             element: (
-              <Suspense>
+              <Suspense fallback={<h1>Loading....</h1>}>
                 <WatchPage />
               </Suspense>
             ),
